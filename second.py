73907b97b8f41fa3895c0a5223e6968cb7ee82a2
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-31 19:00:38 +0330

    sadeghi

--- second.py
+++ second.py
@@ -1,1 +1,23 @@
-print("second file")
+# print("second file")
+
+
+# numbers int    float
+
+# print(1)
+# print(1.2)
+
+x = 1
+
+print(x)
+print(type(x))
+
+
+x = 1.2
+
+print(type(x))
+
+# ناگذاری متغیرها
+
+name1 = 'ali'
+print(type(name1))
+name2 = "reza"
