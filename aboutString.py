b4be223d8604a45f1abfa4e60ced1cd3b6ba7f41
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-07 18:59:48 +0330

    sadeghi

---
+++ aboutString.py
@@ -0,0 +1,29 @@
+# message = "he said \"not do that\""
+# print(message)
+# message = 'he said "not do that"'
+# print(message)
+
+# message = 'he said "don\'nt do that"'
+# print(message)
+
+
+# name = input('enter your name: ')
+# print(name)
+
+# number1 = float(input('enter a number: '))
+# number2 = float(input('enter a number: '))
+
+# output = number1 + number2
+# print("sum is:", output)
+
+
+# message = "hello every body. welcome to our python class.\nwe are going to learn everythings about python."
+# print(message)
+# message = """hello every body. welcome to our python class.
+# we are going to learn everythings about python."""
+# print(message)
+name = input('enter a name: ')
+course = input('enter course name: ')
+message = f"""hello {name}. welcome to our {course} class.
+we are going to learn everythings about {course}."""
+print(message)
