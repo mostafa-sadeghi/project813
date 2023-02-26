ac73c2748243fae8e7c6ad6c374061d39a9479a5
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-26 19:05:00 +0330

    sadeghi

--- tuple.py
+++ tuple.py
@@ -1,8 +1,8 @@
-numbers = ()
-print(type(numbers))
+# numbers = ()
+# print(type(numbers))
 
-numbers = (1, 2, 3, 4, 5)
+# numbers = (1, 2, 3, 4, 5)
 # numbers[0] = 100   # تاپل غیر قابل تغییر است
 # numbers.append(6) # تاپل متد اپند ندارد
-print(numbers[0])
-print(numbers[0:3])
+# print(numbers[0])
+# print(numbers[0:3])
