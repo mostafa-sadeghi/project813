1048792966e48d0f11f67bdf263589d80646734d
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-05 18:58:15 +0330

    sadeghi

--- aboutTurtle.py
+++ aboutTurtle.py
@@ -1,22 +1,16 @@
 import turtle
-
 s = turtle.Screen()
 s.bgcolor('orange')
 s.register_shape('strawberry.gif')
 # s.bgpic('mario.png')
-
 p = turtle.Pen()
-
 # p.shape('square')  # 'arrow', 'turtle', 'circle', 'square', 'triangle', 'classic'
 p.shape('strawberry.gif')
 p.shapesize(1.2)
-
 p.pensize(4)
 p.pencolor('red')
-
 p.fillcolor('green')
 p.begin_fill()
-
 # draw triangle
 p.forward(100)
 p.left(120)
@@ -24,16 +18,13 @@ p.forward(100)
 p.left(120)
 p.forward(100)
 p.left(120)
-
 p.end_fill()
-
 p.forward(100)
 p.right(120)
 p.forward(100)
 p.right(120)
 p.forward(100)
 p.right(120)
-
 s.exitonclick()
 
 
