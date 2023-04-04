1697cf4a89cb48ef30d25d5d0e81fc893f342b7b
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-04 18:55:50 +0330

    sadeghi

---
+++ s16.py
@@ -0,0 +1,75 @@
+a = 4
+b = 4
+
+# if a <= b:
+#     print("one")
+
+# if a >= b:
+#     print("two")
+
+# if a == b:
+#     print("one")
+
+# if a != b:
+#     print("two")
+
+
+# for i in range(3):
+#     print("hi")
+
+# برنامه ای بنویسید که سه عدد از ورودی دریافت نماید و تعداد اعداد زوج وارد شده را نمایش دهد
+# count = 0
+# for i in range(3):
+#     number = int(input('enter a number:> '))
+#     if number % 2 == 0:
+#         count = count + 1
+
+# print("even counts: ", count)
+
+
+# a = 1
+
+# if a == 1:
+#     print("yes")
+#     print("1=1")
+#     print("ok")
+
+
+# a = 1
+# b = 2
+# c = 3
+
+# if a < b and a < c:
+#     print("a is lower than b and c")
+
+# if a < b or a < c:
+#     print("a is lower than b or c")
+
+# hw 1
+'''
+برنامه ای بنویسید که 6 عدد از ورودی دریافت نماید و
+مجموع اعداد زوج و نیز محموع اعداد فرد را جداگانه نمایش دهد
+همچنین اعداد وارد شده را از آخر به اول نمایش دهد
+'''
+# a = True
+# if a:
+#     print("ok")
+
+# if not a:
+#     print("two")
+
+# a = True
+# b = False
+
+# if a and b:
+#     print("a and b are both True")
+
+# if a or b:
+#     print('a or b is true')
+
+# a = 3
+# b = 3
+
+# c = a == b
+
+# print(c)
