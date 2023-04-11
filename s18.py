17014e3c376c94da8020064d74f0c525dab0979c
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-11 19:02:35 +0330

    sadeghi

---
+++ s18.py
@@ -0,0 +1,51 @@
+# i = 0
+# while i < 5:
+#     print("I want to learn pygame....")
+# i = i + 1 # i += 1
+
+
+# quit = 'n'
+# while quit == 'n':
+#     quit = input('Do you want to quit? ')
+
+# done = False
+# while not done:
+#     quit = input('Do you want to quit? ')
+#     if quit == 'y':
+#         done = True
+
+# import random
+
+# for i in range(10):
+#     my_number = random.randrange(100,501)
+#     print(my_number)
+
+# actions = ["rock", "paper", "scissors"]
+
+# random_index = random.randrange(3)
+# print(actions[random_index])
+'''
+player_hand
+دست بازیکن از ورودی پرسیده می شود
+computer_hand
+دست کامپیوتر از طریق رندم مشخص می شود
+
+با کمک شرط ها منطق بازی را پیاده می کنید
+
+از حلقه وایل برای ادامه دار بودن بازی استفاده کنید
+یعنی فقط یک دست نباشد
+بازیکن بعد از هر دست با زدن اینتر دست بعدی را بتواند بازی کند
+با واردی کرد
+quit 
+از بازی خارج شود
+
+می توانید به بازی امتیاز هم اضافه نمائید 
+تا امتیاز شما و کامپیوتر را نمایش دهد
+امتیاز کل
+
+'''
+import pygame
+# Initialize the game engine
+pygame.init()
+
+
