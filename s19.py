73fa8ebd27bd9b39a0a9100924720d71d270acc7
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-25 18:58:32 +0330

    sadeghi

--- s19.py
+++ s19.py
@@ -1,15 +1,31 @@
-import pygame
-pygame.init()
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
+# import pygame
+# pygame.init()
+# BLACK = (0, 0, 0)
+# WHITE = (255, 255, 255)
+# RED = (255, 0, 0)
 
-screen = pygame.display.set_mode((700, 500))
-pygame.draw.rect(screen, RED, [0, 0, 50, 50])
-pygame.display.flip()
+# screen = pygame.display.set_mode((700, 500))
+# clock = pygame.time.Clock()
+# xposition = 0
+# yposition = 0
 
-done = True
-while done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = False
+# done = True
+# while done:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             done = False
+#     pygame.draw.rect(screen, RED, [xposition, yposition, 10, 10])
+#     xposition += 1
+#     yposition += 1
+#     pygame.display.flip()
+#     clock.tick(50)
+
+
+for i in range(5):
+    name = input('enter a name: ')
+    if name == 'amin' or name == "reza":
+        print("ok")
+    elif name == "sara":
+        print("sara")
+    else:
+        print("other")
